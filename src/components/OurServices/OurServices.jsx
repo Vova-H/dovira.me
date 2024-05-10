deace27be75b727ffd01c4ef9b1d892060e8c36c
Volodymyr Havrikov <volodymyr.havrikov@plus4u.net>
2024-05-10 15:49:48 +0300

    added to git OurServices files

---
+++ src/components/OurServices/OurServices.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import classes from "./ourServices.module.css";
+import doctor from "../../assets/images/ourServices/doctor.svg"
+import prescriptions from "../../assets/images/ourServices/prescription.svg"
+import videoConsultation from "../../assets/images/ourServices/video consultation.svg"
+import veterinaryCare from "../../assets/images/ourServices/paw.svg"
+
+const OurServices = () => {
+    return (
+        <div className={classes.container}>
+            <div className={classes.title}>Our services</div>
+            <div className={classes.serviceWrapper}>
+                <div className={classes.service}>
+                    <img src={doctor} alt="doctor" className={classes.serviceImage}/>
+                    <div className={classes.serviceName}>Doctor house calls</div>
+                    <div className={classes.serviceDescription}>
+                        Get a doctor at your door! Use our app to request a
+                        house call, and a qualified professional will come to you, ready to diagnose and treat various
+                        conditions.
+                    </div>
+                </div>
+
+                <div className={classes.service}>
+                    <img src={prescriptions} alt="prescriptions" className={classes.serviceImage}/>
+                    <div className={classes.serviceName}>Prescription services</div>
+                    <div className={classes.serviceDescription}>With Medivisit, get prescriptions from our online
+                        doctors directly in the app. Download them for easy access whenever necessary.
+                    </div>
+                </div>
+
+                <div className={classes.service}>
+                    <img src={videoConsultation} alt="video consultaion" className={classes.serviceImage}/>
+                    <div className={classes.serviceName}>Video consultations</div>
+                    <div className={classes.serviceDescription}>Talk to your doctor from anywhere, perfect for busy
+                        schedules or urgent needs.
+                    </div>
+                </div>
+
+                <div className={classes.service}>
+                    <img src={veterinaryCare} alt="veterinary care" className={classes.serviceImage}/>
+                    <div className={classes.serviceName}>Veterinary care</div>
+                    <div className={classes.serviceDescription}>Medivisit cares for your pets too! Request a vet house
+                        call or schedule a video consultation to ensure your furry friends get the medical attention
+                        they need.
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default OurServices;
