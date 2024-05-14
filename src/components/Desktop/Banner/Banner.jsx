60a150216992aa90837c349ce35aced352cc72cb
Volodymyr Havrikov <volodymyr.havrikov@plus4u.net>
2024-05-14 17:33:33 +0300

    changed picture of doctor from banner section to higher resolution

--- src/components/Desktop/Banner/Banner.jsx
+++ src/components/Desktop/Banner/Banner.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import classes from "./banner.module.css"
 import pictureDoctor from "../../../assets/images/banner/picture of doctor.svg"
-import smallCard from "../../../assets/images/banner/smallCard.png"
-import card from "../../../assets/images/banner/card.png"
 import facebook from "../../../assets/images/banner/facebook.svg"
 import instagram from "../../../assets/images/banner/instagram.svg"
 import linkedIn from "../../../assets/images/banner/linkedin.svg"
@@ -49,12 +47,6 @@ const Banner = () => {
                 <div className={classes.bannerRightSide}>
                     <div className={classes.doctorImgWrapper}>
                         <img src={pictureDoctor} alt="Doctor" className={classes.doctorImg}/>
-                    </div>
-                    <div className={classes.smallCardWrapper}>
-                        <img src={smallCard} alt="Small card" className={classes.smallCard}/>
-                    </div>
-                    <div className={classes.cardWrapper}>
-                        <img src={card} alt="Card" className={classes.card}/>
                     </div>
                 </div>
             </div>
