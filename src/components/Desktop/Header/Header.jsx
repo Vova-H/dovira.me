e8573ced41eb2e830ed2fc06794f7b3e89eb5861
Volodymyr Havrikov <volodymyr.havrikov@plus4u.net>
2024-05-14 15:40:48 +0300

    reworked main container and desktop layout, fixed white lines by sides and removed fixed size of container

--- src/components/Desktop/Header/Header.jsx
+++ src/components/Desktop/Header/Header.jsx
@@ -2,9 +2,7 @@ import React from 'react';
 import classes from './header.module.css';
 import Logo from "../../../assets/images/banner/logo.svg";
 
-
-const Header = ({scrollTo, refs}) => {
-
+const Header = ({ scrollTo, refs }) => {
     const handleNavigationClick = (ref) => {
         scrollTo(ref);
     };
@@ -12,30 +10,25 @@ const Header = ({scrollTo, refs}) => {
     return (
         <header className={classes.headerContainer}>
             <div className={classes.header}>
-                <img className={classes.logo} src={Logo} alt="Logo"/>
+                <img className={classes.logo} src={Logo} alt="Logo" />
                 <div className={classes.navigation}>
-                    <div className={classes.navigationItem}
-                         onClick={() => handleNavigationClick(refs.ourServicesRef)}>Our
-                        services
+                    <div className={classes.navigationItem} onClick={() => handleNavigationClick(refs.ourServicesRef)}>
+                        Our services
                     </div>
-                    <div className={classes.navigationItem}
-                         onClick={() => handleNavigationClick(refs.whyChooseUsRef)}>
+                    <div className={classes.navigationItem} onClick={() => handleNavigationClick(refs.whyChooseUsRef)}>
                         Why choose us
                     </div>
-                    <div className={classes.navigationItem}
-                         onClick={() => handleNavigationClick(refs.ourPrioritiesRef)}>
+                    <div className={classes.navigationItem} onClick={() => handleNavigationClick(refs.ourPrioritiesRef)}>
                         Our priorities
                     </div>
-                    <div className={classes.navigationItem}
-                         onClick={() => handleNavigationClick(refs.downloadAppRef)}>
+                    <div className={classes.navigationItem} onClick={() => handleNavigationClick(refs.downloadAppRef)}>
                         Download the app
                     </div>
-                    <div className={classes.navigationItem}
-                         onClick={() => handleNavigationClick(refs.contactUsRef)}>Contact us
+                    <div className={classes.navigationItem} onClick={() => handleNavigationClick(refs.contactUsRef)}>
+                        Contact us
                     </div>
                 </div>
             </div>
-
         </header>
     );
 };
